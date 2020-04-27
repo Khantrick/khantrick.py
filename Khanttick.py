2f444f7186c024885d86764cd6ce1031e449a325
Khantrick <64401200+Khantrick@users.noreply.github.com>
2020-04-27 14:01:47 +0500

    Update Khanttick.py

--- Khanttick.py
+++ Khanttick.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python2
+#!/usr/bin/python2 Khantrick.py
 #coding=utf-8
 #The Credit For This Code Goes To lovehacker
 #If You Wanna Take Credits For This Code, Please Look Yourself Again...
